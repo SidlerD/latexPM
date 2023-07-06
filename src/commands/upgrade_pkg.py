507e2e7b4db57c2be8dd1bcfd7abe9b09f8db9d8
Dominik Sidler <Dominik.Sidler00@gmail.com>
2023-07-06 13:04:41 +0200

    Upgrade_pkg lalmost finished
    Cannot try it out yet, since that requires other package-source than CTAN

--- src/commands/upgrade_pkg.py
+++ src/commands/upgrade_pkg.py
@@ -5,46 +5,76 @@
 
 import logging
 from src.API import CTAN
+from src.commands.install_pkg import install_pkg
+from src.commands.remove_pkg import remove_pkg
 from src.core.LockFile import LockFile
 from src.models.Dependency import Dependency, DependencyNode
 from src.models.Version import Version
+from src.helpers.DependenciesHelpers import extract_dependencies
 from anytree import Node
 
 logger = logging.getLogger("default")
 
 def _handle_dep(dep: Dependency, root: Node):
     # Get new files from CTAN
+    dep_new = CTAN.download_pkg(dep)
+
     # Get Dependencies of old version from LockFile (i.e direct children of its node)
-    # Get Dependencies of files
-    # For each dependency of new version: Check if old version has same dependencies
-    #   If so, remove from both lists
-    #   If not, leave in list
-    # For each new_dep in new_list: install_pkg(new_dep)
-    # For each old_dep in old_list: remove_pkg(old_dep) # THis should include checking .dependents
+    dep_old = LockFile.is_in_tree(dep, root)
+    if not dep_old: # TODO: Try to find manually by searching packages folder here
+        raise Exception(f"Cannot update {dep}: Is not in Lock-File")
+    deps_of_old = [node.dep for node in dep_old.children if hasattr(node, "dep")]
+    
+    # Get Dependencies of new version of package
+    deps_of_new = extract_dependencies(dep_new)
+            
     # For each pkg in dep.dependents: Check if they are okay with new version
     # If new package isn't okay, idk what to do: 
     #   Maybe warn error about situation and ask if he wants to disregard this and proceed with new version
+    # TODO: Check if any package depends on dep, implement the above
+
+
+    # For each dependency of new version: Check if old version has same dependencies
+    #   If so, remove from both lists
+    #   If not, leave in list
+    to_install = []
+    for d in deps_of_new:
+        if d in deps_of_old:
+            to_install.append(d)
+            deps_of_old.remove(d)
+
+    # For each new_dep in new_list: install_pkg(new_dep)
+    for pkg in to_install:
+        install_pkg(pkg)
+
+    # For each old_dep in old_list: remove_pkg(old_dep) # This should include checking .dependents
+    for pkg in deps_of_old:
+        remove_pkg(pkg)
+
     pass
 
-def upgrade_pkg(pkg_id: str):
+def upgrade_pkg(pkg_id: str, lock_file: LockFile):
     rootNode = None
     try:
         dep = Dependency(pkg_id, CTAN.get_name_from_id(pkg_id))
-        rootNode = LockFile.read_file_as_tree()
+        rootNode = lock_file.read_file_as_tree()
     
         exists = LockFile.is_in_tree(dep, rootNode)
         if not exists:
-            logger.warning(f"Upgrading {pkg_id} not possible: {pkg_id} not found in {LockFile.get_name()}")
+            logger.warning(f"Upgrading {pkg_id} not possible: {pkg_id} not found in {lock_file.get_name()}")
             return
         
         old_version = exists.dep.version if hasattr(exists, 'dep') else Version() #FIXME: What is else case here?
         new_version = CTAN.get_version(pkg_id)
         
+        if(old_version == new_version):
+            logger.warning(f"c{dep} is already in newest version, upgrading not possible")
+            return
+        
         logger.info(f"Upgrading {pkg_id} from {old_version} to {new_version}")
-        _handle_dep(dep, rootNode, rootNode) 
+        _handle_dep(dep, rootNode) 
 
-        LockFile.write_tree_to_file(rootNode)
-        logger.info(info)
+        lock_file.write_tree_to_file(rootNode)
 
     except Exception as e:
         
---
+++ test/integration/upgrade_pkg/test_upgrade_pkg.py
@@ -0,0 +1,44 @@
+import unittest
+from unittest.mock import MagicMock, patch
+from src.models.Dependency import DependencyNode, Dependency
+from src.core.lpm import lpm
+from anytree import Node
+
+from src.models.Version import Version
+
+class UpgradePkgTest(unittest.TestCase):
+
+
+    @patch("src.commands.upgrade_pkg.LockFile.read_file_as_tree")
+    @patch("src.commands.upgrade_pkg.CTAN.get_version")
+    @patch("src.commands.upgrade_pkg.CTAN.get_name_from_id")
+    def test_upgrade_warns_and_returns_if_in_newest_version(self, get_name_mock, get_version_mock, read_as_tree_mock):
+        root = Node('root')
+        dep_version = Version("v1.2a")
+        dep = Dependency('A', '', dep_version)
+        dep_node = DependencyNode(dep, parent=root)
+
+
+        get_name_mock.return_value = ""
+        get_version_mock.return_value = dep_version
+        read_as_tree_mock.return_value = root
+        
+        with self.assertLogs('default', level='WARNING') as cm:
+            lpm_inst = lpm()
+            lpm_inst.upgrade_pkg('A')
+            self.assertEqual(len(cm.output), 1) # Warned once
+            self.assertTrue("already in newest version" in cm.output[0])
+
+    @patch("src.commands.upgrade_pkg.LockFile.read_file_as_tree")
+    @patch("src.commands.upgrade_pkg.CTAN.get_name_from_id")
+    def test_upgrade_warns_if_not_installed(self, get_name_mock, read_as_tree_mock):
+        # dep = Dependency('A', '', "v1.2a")
+
+        get_name_mock.return_value = ""
+        read_as_tree_mock.return_value = Node('root')
+
+        with self.assertLogs('default', level='WARNING') as cm:
+            lpm_inst = lpm()
+            lpm_inst.upgrade_pkg('A')
+            self.assertEqual(len(cm.output), 1) # Warned once
+            self.assertTrue("not possible" in cm.output[0])
