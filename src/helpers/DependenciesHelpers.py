cb142388cf787ae246c080ac57b8d2367d57f92d
Dominik Sidler <Dominik.Sidler00@gmail.com>
2023-07-18 09:57:46 +0200

    Changed dependency extraction drastically
    Before: For depA, look at each included sty-file and extract all their dependencies
    
    Now: For depA, look at depA.sty and extract dependencies. If it depends on other included sty-file, extract dependencies of that one too.
    
    I'm not sure if this is right approach, but for package listings and amsmath, it now downloads exactly those packages whose files are then used in building .tex file. Prev., it downloaded waaay more

--- src/helpers/DependenciesHelpers.py
+++ src/helpers/DependenciesHelpers.py
@@ -1,4 +1,5 @@
 import os
+from os.path import basename, join
 import re
 import logging
 from src.API import CTAN, TexLive
@@ -16,48 +17,46 @@ req_pkg_pattern = r'^\s*(?<!%)\s*\\(?:RequirePackage|usepackage)(?:\[(?:.*?)\])?
 """Captures both RequiresPackage and usepackage. group1 = Pkg_name, group2 = version if available\n
     https://regex101.com/r/BnKbkR/1
 """
+logger = logging.getLogger("default")
 
 def extract_dependencies(dep: DownloadedDependency) -> list[Dependency]:
-    logger = logging.getLogger("default")
     logger.info("Extracting dependencies of " + dep.id)
 
-    deps_of_files = set()
-    included_deps = [] # Files that were included in the download of dep
+    sty_files = [file_name for file_name in dep.files if file_name.endswith('.sty')]
+    file_names = [basename(sty_path).split('.')[0] for sty_path in sty_files]
 
-    sty_files = [os.path.join(dep.path, file_name) for file_name in dep.files if file_name.endswith('.sty')] #TODO: .ins and .tex files
-    
-    # Get names of "packages" included in download
-    for sty_path in sty_files:
+    to_extract = [f'{dep.name}.sty']
+    final_deps: list[Dependency] = []
+
+    while to_extract:
+        sty_name = to_extract.pop()
+        sty_path = join(dep.path, sty_name)
+
+        if not os.path.exists(sty_path):
+            if os.path.exists(os.path.abspath(sty_name)):
+                sty_path = os.path.abspath(sty_name)
+            else:
+                raise RuntimeError(f"Cannot extract dependencies for {dep.id}: {sty_path} does not exist")
+        
         with open(sty_path, "r") as sty:
-            # Dependencies that are already included when downloading package
-            match = re.search(prov_pkg_pattern, sty.read())
-            if match: # Get name from command in sty-file
-                package_name = match.group(1)
-                package_version = match.group(2)
-                included_deps.append(package_name) # Assumption that I dont document dependencies on included files, else I need to pass the version (not just none)
-            else: # Take file-name as package-name
-                package_name = os.path.basename(sty_path).split('.')[0]
-                if(package_name):
-                    included_deps.append(package_name)
-                    logger.debug(f"""File {os.path.basename(sty_path)} doesn't have a ProvidesPackage or ProvidesFile. Had to fallback and use its filename. This could lead to problems""") #TODO: Make this warning, not debug
-    # Get dependencies of files
-    for sty_path in sty_files:
-        with open(sty_path, "r") as sty:
-            matches: list[tuple] = re.findall(req_pkg_pattern, sty.read())
+            content = sty.read()
+            matches: list[tuple] = re.findall(req_pkg_pattern, content, re.MULTILINE)
             for (package_names, package_version) in matches:
                 package_names = package_names.split(',')
                 for name in package_names:
-                    if name in included_deps:
-                        # Sort out deps whose files were included in the download of current dep
-                        # This assumes that when I download package A which depends on (included) fileB, fileB is included in the right version
-                        # Could check for assumption, but it seems versions in \RequirePackage are sometimes outdated and not up-to-date
-                        logger.debug(f"{dep.id} depends on {name}, which was included in its download")
-                        continue
-                    try:
-                        deps_of_files.add(Dependency(CTAN.get_id_from_name(name), name, package_version))
-                        logger.debug(f"Adding {name} as dependency of {dep.id}")
-                    except CtanPackageNotFoundError as e:
-                        logger.warning(f"{os.path.basename(sty_path)} from package {dep.id} depends on {name}, but CTAN has no information about {name}. {name} will not be installed. If problems arise, please install {name} manually.")
+                    # Why not check for ProvidesPackage here?: Latex imports by file name, not by ProvidesPackage. Test with pkgA.sty which \ProvidesPackage{pkgB}. Can only import with \usepackage{pkgA}
+                    if name in file_names: 
+                        # dep requires on other file which was included in download. Since it is needed, we also parse its dependencies
+                        # ASSUMPTION: If file is included in download, it's included in right version. Therefore don't need to check version here
+                        logger.debug(f"{sty_name} depends on {name}, which was included in its download")
+                        to_extract.append(f'{name}.sty')
+                    else:
+                        try:
+                            final_deps.append(Dependency(CTAN.get_id_from_name(name), name, package_version))
+                            logger.debug(f"Adding {name} as dependency of {dep.id}")
+                        except CtanPackageNotFoundError as e:
+                            logger.warning(f"{os.path.basename(sty_name)} from package {dep.id} depends on {name}, but CTAN has no information about {name}. {name} will not be installed. If problems arise, please install {name} manually.")
 
-    logger.info(f"{dep} has {len(deps_of_files)} dependencies: {', '.join([dep.id for dep in deps_of_files])}")
-    return list(deps_of_files)
+    logger.info(f"{dep} has {len(final_deps)} dependencies: {', '.join([dep.id for dep in final_deps])}")
+    return list(final_deps)
+    
