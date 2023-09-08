72d89802ae8ebe2fb5a8a46dc23ba24ea0596dbd
Dominik Sidler <Dominik.Sidler00@gmail.com>
2023-09-08 15:45:08 +0200

    Add version to packages installed from CTAN

--- src/API/CTAN.py
+++ src/API/CTAN.py
@@ -65,6 +65,11 @@ def download_pkg(dep: Dependency, pkgInfo=None) -> DownloadedDependency:
     logger.info(f"CTAN: Installing {dep} from {url}")
     folder_path = download_and_extract_zip(url, dep)
     
+    if 'version' in pkgInfo:
+        version = Version(pkgInfo['version'])
+        dep.version = version
+    else:
+        logger.warn(f"Couldn't find version for {dep}")
     try:
         ctan_path = pkgInfo['ctan']['path']
     except KeyError:
