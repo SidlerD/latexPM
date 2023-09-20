0beb2d7827ec4fa50b21d1ec644b48eb1cdc4778
Dominik Sidler <Dominik.Sidler00@gmail.com>
2023-09-20 18:45:09 +0200

    First working prototype of dep_extraction accuracy measurer

--- thesis/dep_extraction_accuracy/lpm.py
+++ thesis/dep_extraction_accuracy/lpm.py
@@ -1,21 +1,51 @@
+import json
+import os
+from os.path import isfile, join
+import shutil
+import tempfile
 from src.API import CTAN
 
 from src.helpers.DependenciesHelpers import extract_dependencies
 from src.models.Dependency import Dependency
 
+file_name = 'lpm_deps_of_files_output.json'
+
+
 def get_deps_of_pkgs(tlmgr_pkgs):
+    # if os.path.exists(file_name):
+    #     print("Taking data from " + file_name)
+    #     with open(file_name, 'r') as f:
+    #         data = json.load(f)
+        
+    #     return data
+    
     res = []
+    old_cwd = os.getcwd()
+    tmpdir = tempfile.mkdtemp()
+    os.chdir(tmpdir)
+
     for pkg in tlmgr_pkgs:
-        dep = Dependency(pkg['name'], pkg['name'])
+        try:
+            dep = Dependency(pkg['name'], pkg['name'])
 
-        downloaded_dep = CTAN.download_pkg(dep)
+            downloaded_dep = CTAN.download_pkg(dep)
+            downloaded_dep.files = [elem for elem in os.listdir(downloaded_dep.path) if isfile(join(downloaded_dep.path, elem))]
 
-        deps = extract_dependencies(downloaded_dep)
-        res.append({
-            'name': dep.name,
-            'cat-date': str(downloaded_dep.version.date),
-            'cat-version': downloaded_dep.version.number,
-            'depends': [dep.name for dep in deps]
-        })
+            deps = extract_dependencies(downloaded_dep)
+            res.append({
+                'name': dep.name,
+                'cat-date': str(downloaded_dep.version.date),
+                'cat-version': downloaded_dep.version.number,
+                'depends': [dep.name for dep in deps]
+            })
+        except Exception as e:
+            print(e)
+
+    os.chdir(old_cwd)
+    shutil.rmtree(tmpdir)
+
+    with open(file_name, 'w') as f:
+        json.dump(res, f, indent=2)
 
     return res
+
--- thesis/dep_extraction_accuracy/main.py
+++ thesis/dep_extraction_accuracy/main.py
@@ -1,5 +1,8 @@
+import sys
 from thesis.dep_extraction_accuracy import lpm
-import tlmgr
+from thesis.dep_extraction_accuracy import tlmgr
+import pandas as pd
+
 
 def get_required_pkgs():
     latex_tools = ["afterpage", "array", "bm", "calc", "dcolumn", "delarray", "enumerate", "fileerr", "fontsmpl", "ftnright", "hhline", "indentfirst", "layout", "longtable", "multicol", "rawfonts", "shellesc", "showkeys", "somedefs", "tabularx", "theorem", "trace", "varioref", "verbatim", "xr", "xspace"]
@@ -8,8 +11,41 @@ def get_required_pkgs():
 
     return latex_tools + latex_graphics + amslatex
 
+def get_diff(df: pd.DataFrame, name: str, pkg_deps_lpm: list[str], pkg_deps_tlmgr: list[str]) -> None:
+    required_pkgs = set(get_required_pkgs())
+
+    # Remove packages that are in bundle "required" since they can be assumed to be present
+    lpm = set(pkg_deps_lpm) - required_pkgs
+    tlmgr = set(pkg_deps_tlmgr) - required_pkgs
+
+    identical = lpm.intersection(tlmgr)
+    lpm_over = lpm - tlmgr
+    lpm_under = tlmgr - lpm
+
+    df.loc[len(df)] = {
+        'name': name,
+        'lpm_deps': ';'.join(lpm),
+        'tlmgr_deps': ';'.join(tlmgr),
+        'identical': len(identical),
+        'lpm_over': len(lpm_over),
+        'lpm_under': len(lpm_under)
+    }
+
 def compare_deps(tlmgr_pkgs, lpm_pkgs):
-    required_pkgs = get_required_pkgs()
+
+    df = pd.DataFrame(
+        {
+            'name': [],
+            'lpm_deps': [],
+            'tlmgr_deps': [],
+            'identical': [],
+            'lpm_over': [],
+            'lpm_under': []
+        }
+    )
+    # df.set_index('name', inplace=True)
+   
+    df.head()
 
     # Iterate over lpm_deps because it is a subset of tlmgr_deps
     for pkg in lpm_pkgs:
@@ -21,15 +57,34 @@ def compare_deps(tlmgr_pkgs, lpm_pkgs):
         pkg_tlmgr = next((item for item in tlmgr_pkgs if item["name"] == pkg_name), None)
         pkg_deps_tlmgr = pkg_tlmgr['depends'] if pkg_tlmgr else None
 
-        
+        get_diff(df, pkg_name, pkg_deps_lpm, pkg_deps_tlmgr)
 
+    return df
 
-def measure_accuracy():
+def measure_accuracy(pkgs: list[str] = None):
     tlmgr_pkgs = tlmgr.get_pkgs_with_deps()
+    if pkgs:
+        tlmgr_pkgs = [pkg for pkg in tlmgr_pkgs if pkg['name'] in pkgs]
     lpm_pkgs = lpm.get_deps_of_pkgs(tlmgr_pkgs)
 
-    compare_deps(tlmgr_pkgs, lpm_pkgs)
+    return compare_deps(tlmgr_pkgs, lpm_pkgs)
 
 
 if __name__ == "__main__":
-    measure_accuracy()
+    if len(sys.argv) > 1:
+        pkgs = sys.argv[1:]
+    else:
+        # Build for packages that are included in texlive-latex-recommended: https://tex.stackexchange.com/a/504566/293514
+        pkgs = ["anysize", "beamer", "booktabs", "breqn", "caption", "cite", "cmap", "crop", "ctable", "eso-pic", "euenc", "euler", "etoolbox", "extsizes", "fancybox", "fancyref", "fancyvrb", "filehook", "float", "fontspec", "fp", "index", "jknapltx", "koma-script", "latexbug", "l3experimental", "l3kernel", "l3packages", "lineno", "listings", "lwarp", "mathspec", "mathtools", "mdwtools", "memoir", "metalogo", "microtype", "ms", "ntgclass", "parskip", "pdfpages", "polyglossia", "powerdot", "psfrag", "rcs", "sansmath", "section", "seminar", "sepnum", "setspace", "subfig", "textcase", "thumbpdf", "translator", "typehtml", "ucharcat", "underscore", "unicode-math", "xcolor", "xkeyval", "xltxtra", "xunicode"]
+    
+    res = measure_accuracy(pkgs)
+
+    
+    # Not sure if the context manager is actually needed
+    with pd.option_context("max_colwidth", 1000):
+        res_short = res.drop(['lpm_deps', 'tlmgr_deps'], axis=1)
+        print(res_short)
+
+        # res.to_csv('dep_comparison.csv', index=False)
+        res.style.to_latex('dep_comparison.tex')
+        res_short.style.to_latex('dep_comparison.short.tex')
