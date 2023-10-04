4c706738a403748a1c2eccd30c0ef6a214d1a0f6
Dominik Sidler <Dominik.Sidler00@gmail.com>
2023-10-04 19:13:14 +0200

    Prototype of timing package
    Still need to execute from main.py
    Add explanation on what to do with .dat files
    etc.

---
+++ thesis/timing/main.py
@@ -0,0 +1,18 @@
+import cProfile
+import sys
+import time
+
+from thesis.timing import m_init, m_install_amsmath, m_install_with_deps, m_build
+
+def main(n):
+    m_init.measure(n)
+    m_install_amsmath.measure(n)
+    m_install_with_deps.measure(n)
+    m_build.measure(n)
+    
+
+if __name__ == '__main__':
+    n = sys.argv[1] if len(sys.argv) == 2 else 30
+    
+    main(n)
+    
