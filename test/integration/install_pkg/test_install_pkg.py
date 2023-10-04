c3967b8d253608805d69e5bf703e92f302412784
Dominik Sidler <Dominik.Sidler00@gmail.com>
2023-10-04 19:22:26 +0200

    Expand with more cases for parameterized
    Reset lockfile after each test-case execution

--- test/integration/install_pkg/test_install_pkg.py
+++ test/integration/install_pkg/test_install_pkg.py
@@ -3,6 +3,7 @@ import shutil
 import unittest
 from unittest.mock import patch, call
 from src.models.Dependency import Dependency, DownloadedDependency
+from src.core import LockFile
 from src.core.lpm import lpm
 from anytree import Node
 import tempfile
@@ -27,6 +28,8 @@ class InstallPkgTest(unittest.TestCase):
 
         # Remove the directory after the test
         shutil.rmtree(self.test_dir)
+
+        LockFile._root = None
 
     @patch("src.commands.install_pkg.LockFile.read_file_as_tree")  # path to actual file because mocked methods are static
     @patch("src.commands.install_pkg.LockFile.write_tree_to_file")  # path to actual file because mocked methods are static
@@ -59,33 +62,52 @@ class InstallPkgTest(unittest.TestCase):
             self.assertEqual(install_spec_pkg_mock.call_count, 2)
             install_spec_pkg_mock.assert_has_calls([call(depA, accept_prompts=True), call(depB, accept_prompts=True)], any_order=False)
 
+
     @parameterized.expand([
-        ["amsmath", '\\begin{equation*}\n  a=b\n\\end{equation*}'],
-        [ "listings", '\\begin{lstlisting}\n    import numpy as np\n\\end{lstlisting}'],
-        ["tikz", '\\begin{tikzpicture}\n    \\filldraw[color=red!60, fill=red!5, very thick](-1,0) circle (1.5);\n\\end{tikzpicture}']
+    #   [pkg_name,      text]
+        ["amsmath",     '\\begin{equation*}\n  a=b\n\\end{equation*}'],
+        [ "listings",   '\\begin{lstlisting}\n    import numpy as np\n\\end{lstlisting}'],
+        ["tikz",        '\\begin{tikzpicture}\n    \\filldraw[color=red!60, fill=red!5, very thick](-1,0) circle (1.5);\n\\end{tikzpicture}'],
+        ["hyperref",    'The following link is clickable thanks to hyperref: \\url{http://stackoverflow.com/}'],
+        ["multirow",    '\\begin{tabular}{c|c}\n\\multicolumn{2}{c}{spans two rows}\\\\ \n 1 & 2 \\\\ \n\\end{tabular}'],
+        ["todonotes",   '\\todo{This is a todo}'],
+        ["siunitx",     'Scientific number: \\num{.3e45}'],
     ])
     @patch("src.API.CTAN.input")
     @patch("src.core.PackageInstaller.input")
     def test_install_packages_and_build_file(self, pkg_name, text, install_closest_version_input, build_aliases_file_input):
+        os.chdir(self.test_dir)
+        self.assertFalse(os.path.exists('file.pdf'))
+
         install_closest_version_input.return_value = 'y'
         build_aliases_file_input.return_value = 'n'
 
-        os.chdir(self.test_dir)
-        with open('file.tex', 'w') as f:
-            f.write('\n'.join([
+
+        # Create the file that uses the package
+        file_content = '\n'.join(
+            [
                 r'\documentclass{article}',
                 '\\usepackage{%s}' % pkg_name,
                 r'\begin{document}',
                 text,
                 r'\end{document}'
-            ]))
+            ])
+        
+        with open('file.tex', 'w') as f:
+            f.write(file_content)
+        
+        # Initialize a project with lpm
         lpm_inst = lpm()
         lpm_inst.init(docker_image='registry.gitlab.com/islandoftex/images/texlive:TL2023-2023-08-20-small')
 
-        lpm_inst.install_pkg(pkg_name)
+        # Install the needed package
+        lpm_inst.install_pkg(pkg_name, accept_prompts=True)
 
+        # Build the project
         lpm_inst.build(['pdflatex', 'file.tex'])
+        lpm_inst.remove(pkg_name)
 
+        # Print log-file(s)
         if os.path.exists('file.log'):
             with open('file.log', 'r') as log:
                 print(log.read())
@@ -101,4 +123,5 @@ class InstallPkgTest(unittest.TestCase):
                     print(f"==== {file} =====")
                     print(log.read())
 
+        # Check that pdf was created
         self.assertTrue(os.path.exists('file.pdf'))
